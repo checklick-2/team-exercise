f95f57c84bb64bdf64013db1eb7af1c2ef8253e4
Owen Murovec <owenmurovec@gmail.com>
2021-09-18 17:49:51 -0700

    Create Owen Section

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import Owen from "./components/owen";
 import "./App.css";
 
 function App() {
@@ -7,6 +8,7 @@ function App() {
         <h1>Checklick 2</h1>
         <h2>Meet the team</h2>
       </header>
+      <Owen />
     </div>
   );
 }
